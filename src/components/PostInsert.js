b883e6009f6c8f3c5fe6577ce0f4b6f8cf83e10e
chvgf <chvgf@naver.com>
2023-11-07 21:41:10 +0900

    postListInsert

--- src/components/PostInsert.js
+++ src/components/PostInsert.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const PostInsertWrapper = styled.form`
@@ -8,21 +8,68 @@ const PostInsertWrapper = styled.form`
   display: flex;
   flex-flow: column;
 
-  input {
+  input, select {
     height: 100px;
     background-color: beige;
     margin-bottom: 30px;
     border: none;
-    font-size: 24px;
+    font-size: 20px;
   }
 `;
+
+
 function PostInsert(props) {
+  const [title, setTitle] = useState('');
+  const titleChange = (e) => {
+    return setTitle(e.target.value)
+  }
+
   return (
     <PostInsertWrapper>
-      <input type='text' />
-      <input type='text' />
-      <input type='date' />
-      <input type='text' placeholder='장소 api 되면 업데이트' /> {/* 장소 api 업데이트 */}
+      <label htmlFor='1'>제목</label>
+      <input
+        id='1'
+        type='text'
+        value={title}
+        onChange={() => titleChange()}
+        />
+      <label htmlFor='2'>내용</label>
+      <input 
+        id='2'
+        type='text'
+      />
+      <label htmlFor='3'>장소</label>   {/* 장소 api */}
+      <input 
+        id='3'
+        type='text'
+      />
+      <label htmlFor='4'>성별</label>
+        <select
+          id='4'
+        >
+          <option>남</option>
+          <option>여</option>
+        </select>
+      <label htmlFor='5'>참여 인원</label>
+      <select
+          id='5'
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+        </select>
+      <label htmlFor='6'>경기 방식</label>
+      <select
+          id='6'
+        >
+          <option>단식</option>
+          <option>복식</option>
+        </select>
     </PostInsertWrapper>
   );
 }
