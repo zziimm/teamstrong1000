3e37b0c982b469d625055d3783d13fe9435f835c
zziimm <wlalscjs30@naver.com>
2023-12-18 17:49:16 +0900

    승리 / 패배 요청 유효성 검사
    프론트: 한번 요청을 보내면 더이상 요청 보낼 수 없음,
    복식: 같이 승리한 팀원은 버튼이 '승리'로 나오고 그렇지 않은 사람들은 '패배'로 나옴
    
    전부 입력 하면 경기 경과 반영 후 경기삭제 + 승패 여부에 따라 스코어 변동

--- src/components/MypageMatchItem.js
+++ src/components/MypageMatchItem.js
@@ -58,12 +58,12 @@ const Area = styled.div`
 
 function MypageMatchItem(props) {
   console.log(props);
-  const { title, district, joinPersonnel, joinMember, game, selectDate, postId, lo, check } = props
+  const { title, district, joinPersonnel, joinMember, game, userId, selectDate, postId, lo, check, win, reFresh, reFreshConfirm } = props
   const [popup, setPopup] = useState(false);
   const [hotNews, setHotNews] = useState(false);
-  const [newsCheck, setNewsCheck] = useState(check);
   const loginUser = useSelector(getLoginUser);
-  
+  const [newsCheck, setNewsCheck] = useState(check);
+  const [isWin, setIsWin] = useState(win);
   const handlePopup = () => {
     if (lo === 'red') {
       return setHotNews(true)
@@ -72,6 +72,7 @@ function MypageMatchItem(props) {
   };
   const handlClose = () => {
     setPopup(false);
+    setHotNews(false);
   };
   const handleCancelBtn = async () => {
     const result = await axios.post(`${process.env.REACT_APP_ADDRESS}/myPage/cancelMatch`, { postId }, { withCredentials: true });
@@ -79,26 +80,33 @@ function MypageMatchItem(props) {
       alert(result.data.message);
     }
     alert(result.data.message);
-    setPopup(false)
+    reFresh(result.data.data)
+    setPopup(false);
   };
 
   const handleWinBtn = async () => {
     const withOutMe = joinMember.filter(member => member !== loginUser.userId);
+
+    let result = '';
     if (game === "단식") {
+      // 결과값을 꼭 담아야하면
+      // for (const member of withOutMe) {
+      //   result = await axios.post(`${process.env.REACT_APP_ADDRESS}/myPage/winAlert`, { member, postId, game }, { withCredentials:true });
+      // };
       await withOutMe.forEach(member => axios.post(`${process.env.REACT_APP_ADDRESS}/myPage/winAlert`, { member, postId, game }, { withCredentials:true }));
       alert('결과기록 요청을 보냈습니다!');
+      setNewsCheck(true);
     } else {
       const winMemberGet = prompt('함께 승리한 팀원의 아이디를 적어주세요!', '');
       const winMember = joinMember.filter(member => member == winMemberGet)[0];
-
-
+      
       if (winMember != winMemberGet) {
         alert('정확한 아이디를 입력해주세요!');
       } else {
         await withOutMe.forEach(member => axios.post(`${process.env.REACT_APP_ADDRESS}/myPage/winAlert`, { member, postId, game, winMember }, { withCredentials:true }));
         alert('결과기록 요청을 보냈습니다!');
+        setNewsCheck(true);
       }
-
     }
 
   };
@@ -107,12 +115,14 @@ function MypageMatchItem(props) {
     const result = await axios.post(`${process.env.REACT_APP_ADDRESS}/myPage/matchResult`, { postId }, { withCredentials: true });
     if (result.data.readyFlag) {
       alert(result.data.message);
-      setNewsCheck(check);
+      setNewsCheck(true);
     } 
     if (result.data.flag) {
       alert(result.data.message);
-      setNewsCheck(check);
+      setNewsCheck(true);
     }
+    reFreshConfirm(result.data);
+    setHotNews(false);
   };
   // 핫뉴스 받은 유저의 뉴스값 날리고(postId)
   // 해당 게시글 삭제시키거
@@ -125,29 +135,64 @@ function MypageMatchItem(props) {
         <p>{title}</p>
         <p>{district}</p>
         <p>{joinPersonnel}</p>
+        {joinMember.forEach(member => <p>{member} </p>)}
         <p>{game}</p>
         <p>{selectDate}</p>
         {lo === 'red' 
           ? <p className='alert'>상대의 결과기록 요청이 있습니다!</p>
           : ''
         }
+        {/* {newsCheck ? <p className='alert'>결과 확정 요청을 보냈습니다!</p> : ''} */}
       </div>
-      { hotNews ?
-        <div className='popupBox'>
-          {newsCheck === 1
-            ? <button>확인완료</button>
-            : <button onClick={confirmBtn}>패배</button>
-          }
-          
-        </div>
-        : popup 
+      { hotNews 
         ?
-        <div className='popupBox'>
-          <button onClick={handleWinBtn}>승리</button>
-          <button onClick={handleCancelBtn}>불참하기</button>
-          <button onClick={handlClose}>닫기</button>
-        </div>
-        : ''
+          <div className='popupBox'>
+            {newsCheck
+              ? 
+                <>
+                  <button>확인완료</button>
+                  <button onClick={handlClose}>닫기</button>
+                </>
+              : isWin == 'win'
+                ? 
+                <>
+                  <button onClick={confirmBtn}>승리</button>
+                  <button onClick={handlClose}>닫기</button>
+                </>
+                : 
+                <>
+                  <button onClick={confirmBtn}>패배</button>
+                  <button onClick={handlClose}>닫기</button>
+                </>
+            }
+            
+          </div>
+        : 
+          popup 
+            ?
+              <div className='popupBox'>
+                {newsCheck
+                  ? 
+                    <>
+                      <button>요청 완료</button>
+                      <button onClick={handlClose}>닫기</button>
+                    </>
+                  : loginUser.userId == userId
+                    ?
+                      <>
+                        <button onClick={handleWinBtn}>승리</button>
+                        <button onClick={handlClose}>닫기</button>
+                      </>
+                    :
+                      <>
+                        <button onClick={handleWinBtn}>승리</button>
+                        <button onClick={handleCancelBtn}>불참하기</button>
+                        <button onClick={handlClose}>닫기</button>
+                      </>
+                }
+                
+              </div>
+            : ''
       }
     </Area>
   );
